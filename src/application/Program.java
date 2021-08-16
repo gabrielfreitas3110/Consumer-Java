5435b6357be7631efb40fabe58e7a3f23ca2c71f
Gariel Augusto <gabriel.freitas3110@gmail.com>
2021-08-16 12:31:53 -0300

    Consumer implemnts example

---
+++ src/application/Program.java
@@ -0,0 +1,25 @@
+package application;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+
+import entities.Product;
+import util.PriceUpdate;
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		Locale.setDefault(Locale.US);
+		List<Product> list = new ArrayList<>();
+		list.add(new Product("TV", 900.0));
+		list.add(new Product("sMouse", 50.0));
+		list.add(new Product("Tablet", 350.0));
+		list.add(new Product("HD Case", 80.90));
+		
+		list.forEach(new PriceUpdate());
+		
+		list.forEach(System.out::println);
+	}
+}
