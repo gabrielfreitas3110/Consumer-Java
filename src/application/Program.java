88861aabcbbfd56c473f972c861cd4d04d7a7684
Gariel Augusto <gabriel.freitas3110@gmail.com>
2021-08-16 12:38:34 -0300

    Static method

--- src/application/Program.java
+++ src/application/Program.java
@@ -18,7 +18,8 @@ public class Program {
 		list.add(new Product("Tablet", 350.0));
 		list.add(new Product("HD Case", 80.90));
 		
-		list.forEach(new PriceUpdate());
+		//list.forEach(new PriceUpdate());
+		list.forEach(Product::staticPriceUpdate);
 		
 		list.forEach(System.out::println);
 	}
--- src/entities/Product.java
+++ src/entities/Product.java
@@ -19,6 +19,9 @@ public class Product {
 	public void setPrice(Double price) {
 		this.price = price;
 	}
+	public static void staticPriceUpdate(Product p) {
+		p.setPrice(p.getPrice() * 1.1);
+	}
 	@Override
 	public String toString() {
 		return "Product [name="+name+", price="+String.format("%.2f",price)+']';
